0f739814622a951a6b7d66e9539dd492efa09d7f
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-01-04 21:34:20 -0300

    ..

--- src/functions/update-user/index.ts
+++ src/functions/update-user/index.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { UpdateUser } from "../../types/types";
 
 export const updateUserFunction = async ({ email, domain, domainKey, newEmail, newPassword }: UpdateUser) => {
+
     const request = await axios.post("https://login-api-dev.herokuapp.com/updateuser", {
         email: email,
         domain: domain,
